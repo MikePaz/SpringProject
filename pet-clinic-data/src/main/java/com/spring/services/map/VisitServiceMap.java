3a75c89849fd68e0b65855eec5273a511343d130
Mike Pazarlis <mike.pazarlis@gmail.com>
2019-09-30 17:11:04 +0300

    Correcting saving method in VisitServiceMap

--- pet-clinic-data/src/main/java/com/spring/services/map/VisitServiceMap.java
+++ pet-clinic-data/src/main/java/com/spring/services/map/VisitServiceMap.java
@@ -5,7 +5,7 @@ import com.spring.services.VisitService;
 
 import java.util.Set;
 
-public class VisitServceMap extends AbstractMapService<Visit, Long> implements VisitService {
+public class VisitServiceMap extends AbstractMapService<Visit, Long> implements VisitService {
     @Override
     public Set<Visit> findAll() {
         return super.findAll();
@@ -23,7 +23,11 @@ public class VisitServceMap extends AbstractMapService<Visit, Long> implements V
 
     @Override
     public Visit save(Visit object) {
-        return super.save(object);
+        if(object.getPet() == null || object.getPet().getOwner() == null
+                || object.getPet().getId() == null) {
+            throw new RuntimeException("invalid visit");
+        }
+        return  super.save(object);
     }
 
     @Override
